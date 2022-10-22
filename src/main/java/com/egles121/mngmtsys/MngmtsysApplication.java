1652fd546a48bf674497ded39b9891b089588f3a
egle.simniskyte <egle.simniskyte@accenture.com>
2022-10-22 17:53:51 +0300

    Initial commit

---
+++ src/main/java/com/egles121/mngmtsys/MngmtsysApplication.java
@@ -0,0 +1,13 @@
+package com.egles121.mngmtsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MngmtsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MngmtsysApplication.class, args);
+	}
+
+}
