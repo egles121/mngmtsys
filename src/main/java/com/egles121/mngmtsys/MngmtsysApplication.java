988be6410738b9489942fd788a23dad63feb3622
egle.simniskyte <egle.simniskyte@accenture.com>
2022-10-23 13:15:12 +0300

    Add Swagger link logging in console

--- src/main/java/com/egles121/mngmtsys/MngmtsysApplication.java
+++ src/main/java/com/egles121/mngmtsys/MngmtsysApplication.java
@@ -1,13 +1,24 @@
 package com.egles121.mngmtsys;
 
-import org.springframework.boot.SpringApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
+@Slf4j
 public class MngmtsysApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MngmtsysApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext run = new SpringApplicationBuilder(MngmtsysApplication.class)
+                .bannerMode(Banner.Mode.LOG)
+                .run(args);
+
+        ConfigurableEnvironment configurableEnvironment = run.getEnvironment();
+        String serverPort = configurableEnvironment.getProperty("local.server.port");
+        log.info("Swagger: http://localhost:{}{}", serverPort, configurableEnvironment.getProperty("springdoc.swagger-ui.path"));
+    }
 
 }
